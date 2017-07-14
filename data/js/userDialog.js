8d51a732f7f5d27fa41cbdf4b30a61e7323c1182
andreaxiang <xiangxue89@163.com>
2017-07-14 17:53:30 +0800

    添加登录注册功能

---
+++ data/js/userDialog.js
@@ -0,0 +1,55 @@
+//原生js实现tab切换
+function Tab(ct) {
+  this.ct = ct;
+  this.init();
+  this.bind();
+}
+Tab.prototype.init = function () {
+  this.tabLis = this.ct.querySelectorAll(".tabs>li");
+  this.tabPanels = this.ct.querySelectorAll(".content>li");
+};
+
+Tab.prototype.bind = function () {
+  var _this = this;
+  this.tabLis.forEach(function (tabli) {
+    tabli.onclick = function (e) {
+      var target = e.target;
+      var index = [].indexOf.call(_this.tabLis, target);
+      _this.tabLis.forEach(function (li) {
+        li.classList.remove("active");
+      });
+      target.classList.add("active");
+
+      _this.tabPanels.forEach(function (panel) {
+        panel.classList.remove("active");
+      });
+      _this.tabPanels[index].classList.add("active");
+    };
+  });
+};
+
+var tab1 = new Tab(document.querySelectorAll(".wrap")[0]);
+
+
+
+
+
+
+/*
+var $btn = $(".signIn");
+var $modal = $(".dialog-overlay");
+var $dialog = $(".dialog-box");
+var $close = $(".btn-close");
+
+$btn.addEventListener("click",function(){
+  $modal.style.display = "block";
+  $dialog.style.display = "block";
+});
+$close.addEventListener("click",function(){
+  $modal.style.display = "none";
+  $dialog.style.display = "none";
+});
+$modal.addEventListener("click",function(){
+  $modal.style.display = "none";
+  $dialog.style.display = "none";
+});*/
