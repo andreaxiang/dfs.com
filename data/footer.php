313e0952ef41694e82d471f9b2cb84f4a704e69e
andreaxiang <xiangxue89@163.com>
2017-07-15 11:27:28 +0800

    功能点1：待页面加载完成后，异步请求页头和页尾

---
+++ data/footer.php
@@ -0,0 +1,67 @@
+<?php
+header('Content-Type: text/html;charset=UTF-8');
+?>
+<!--页面底部! -->
+<footer id="footer">
+  <!--购物指南、配送方式等! -->
+  <div id="foot_box" class="clear">
+    <ul>
+      <li><b>购物指南</b></li>
+      <li><a href="#">购物流程</a></li>
+      <li><a href="#">会员介绍</a></li>
+      <li><a href="#">团购/代购</a></li>
+      <li><a href="#">常见问题</a></li>
+      <li><a href="#">大宗订单</a></li>
+      <li><a href="#">联系客服</a></li>
+    </ul>
+    <ul>
+      <li><b>配送方式</b></li>
+      <li><a href="#">上门自提</a></li>
+      <li><a href="#">211限时达</a></li>
+      <li><a href="#">配送服务查询</a></li>
+      <li><a href="#">配送费收取标准</a></li>
+      <li><a href="#">海外配送</a></li>
+    </ul>
+    <ul>
+      <li><b>支付方式</b></li>
+      <li><a href="#">货到付款</a></li>
+      <li><a href="#">在线支付</a></li>
+      <li><a href="#">分期付款</a></li>
+      <li><a href="#">邮局汇款</a></li>
+      <li><a href="#">公司转账</a></li>
+    </ul>
+    <ul>
+      <li><b>售后服务</b></li>
+      <li><a href="#">售后政策</a></li>
+      <li><a href="#">价格保护</a></li>
+      <li><a href="#">退款说明</a></li>
+      <li><a href="#">返修/退换货</a></li>
+      <li><a href="#">取消订单</a></li>
+    </ul>
+    <ul>
+      <li><b>商家服务</b></li>
+      <li><a href="#">商户入驻</a></li>
+      <li><a href="#">我是代购员</a></li>
+      <li><a href="#">商家中心</a></li>
+      <li><a href="#">大丰收规则</a></li>
+      <li><a href="#">配送补贴</a></li>
+    </ul>
+    <ul class="app-download">
+      <li><img class="down-app" width="148" height="148"/></li>
+      <li><b>微信公众号</b></li>
+    </ul>
+    <ul class="app-download">
+      <li><img class="down-app" width="148" height="148"/></li>
+      <li><b>APP扫码下载</b></li>
+    </ul>
+  </div>
+  <div class="copyright">深网公安备案证字第4403300900570 | 粤ICP备14041947号 | Copyright © 2014-2017 深圳市五谷网络科技有限公司 版权所有 <br/>
+    <a href="#">首页</a> <a href="#">五谷科技官网</a> <a href="#">关于我们</a> <a href="#">人才招聘</a>
+  </div>
+  <div class="authentication">
+    <a><img class="ft-1" width="112" height="40"/></a>
+    <a><img class="ft-2" width="108" height="40"/></a>
+    <a><img class="ft-3" width="108" height="40"/></a>
+    <a><img class="ft-4" width="108" height="40"/></a>
+  </div>
+</footer>
---
+++ js/product-list.js
@@ -0,0 +1,5 @@
+/**功能点1：待页面加载完成后，异步请求页头和页尾**/
+$(function(){
+  $('#site_header').load('data/header.php');
+  $('#site_footer').load('data/footer.php');
+})
